7f51aa72dff04b4680627f4bc4deb6712b868d9b
jake-hh <jakob.holzer.m@gmail.com>
2025-11-06 18:48:02 +0100

    [ServiceView] Add menubar to `operationItem`s

--- src/main/java/com/example/application/views/service/ServiceView.java
+++ src/main/java/com/example/application/views/service/ServiceView.java
@@ -8,7 +8,10 @@ import com.example.application.views.MainLayout;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 //import com.vaadin.flow.data.value.ValueChangeMode;
@@ -82,7 +85,6 @@ public class ServiceView extends VerticalLayout {
 
 	private HorizontalLayout getEventItem(Event event) {
 		var eventItem = new HorizontalLayout();
-		//eventItem.setWidthFull();
 		eventItem.setAlignItems(Alignment.START);
 		eventItem.setPadding(true);
 		eventItem.setSpacing(true);
@@ -112,15 +114,20 @@ public class ServiceView extends VerticalLayout {
 		List<Operation> operations = service.findAllOperations(event);
 
 		for (Operation operation : operations) {
-			operationList.add(getOperationItem(operation));
+			operationList.add(getOperationItem(operation, operationList, event));
 		}
 
 		eventItem.add(dateField, mileageField, operationList);
 		return eventItem;
 	}
 
-	private ComboBox<Tracker> getOperationItem(Operation operation) {
-		ComboBox<Tracker> trackerBox = new ComboBox<>("Tracker");
+	private HorizontalLayout getOperationItem(Operation operation, VerticalLayout operationList, Event event) {
+		var operationItem = new HorizontalLayout();
+		operationItem.setAlignItems(Alignment.END);
+		operationItem.setSpacing(true);
+
+		// --- Tracker ComboBox ---
+		var trackerBox = new ComboBox<Tracker>("Tracker");
 		trackerBox.setItems(trackers);
 		trackerBox.setItemLabelGenerator(Tracker::getName);
 		trackerBox.setValue(operation.getTracker());
@@ -129,7 +136,35 @@ public class ServiceView extends VerticalLayout {
 			operation.setTracker(ev.getValue());
 			service.saveOperation(operation);
 		});
-		return trackerBox;
+
+		// --- Icon buttons ---
+		var menuBar = new HorizontalLayout();
+		menuBar.setSpacing(false);
+		menuBar.setPadding(false);
+
+		var addButton = new Button(new Icon(VaadinIcon.PLUS));
+		addButton.addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY);
+		addButton.getElement().setAttribute("title", "Add new operation");
+
+		addButton.addClickListener(e -> {
+			// Add new operation to GUI list but don't save it in db, it will be saved when user sets the tracker
+			Operation newOperation = new Operation();
+			newOperation.setEvent(event);
+			operationList.add(getOperationItem(newOperation, operationList, event));
+		});
+
+		var removeButton = new Button(new Icon(VaadinIcon.TRASH));
+		removeButton.addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_ERROR, ButtonVariant.LUMO_TERTIARY);
+		removeButton.getElement().setAttribute("title", "Remove this operation");
+
+		removeButton.addClickListener(e -> {
+			service.deleteOperation(operation);
+			operationList.remove(operationItem);
+		});
+
+		menuBar.add(addButton, removeButton);
+		operationItem.add(trackerBox, menuBar);
+		return operationItem;
 	}
 
 	/*
