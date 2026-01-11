81a0aeee58cd90716fd232fa4b66d43853f1a4db
jake-hh <jakob.holzer.m@gmail.com>
2026-01-11 05:36:53 +0100

    [Notify] Make messages more readable

--- src/main/java/com/example/application/Notify.java
+++ src/main/java/com/example/application/Notify.java
@@ -6,19 +6,23 @@ import com.vaadin.flow.component.notification.NotificationVariant;
 public class Notify {
 
 	public static void ok(String msg) {
+		//System.out.println("[Ok]    " + msg);
 		var notif = Notification.show(msg);
+		//notif.setDuration(15_000);
 		notif.addThemeVariants(NotificationVariant.LUMO_CONTRAST);
 	}
 
 	public static void debug(String msg) {
-		System.out.println(msg);
+		System.out.println("[Debug] " + msg);
 		var notif = Notification.show(msg);
+		notif.setDuration(15_000);
 		notif.addThemeVariants(NotificationVariant.LUMO_PRIMARY);
 	}
 
 	public static void error(String msg) {
-		System.err.println(msg);
+		System.err.println("[Error] " + msg);
 		var notif = Notification.show(msg);
+		notif.setDuration(15_000);
 		notif.addThemeVariants(NotificationVariant.LUMO_ERROR);
 	}
 }
