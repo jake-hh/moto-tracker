32efd5068025460dcc9712d0486dbc0e7946f6f8
jake-hh <jakob.holzer.m@gmail.com>
2026-02-20 00:25:25 +0100

    [VehicleType] Reorder vehicles

--- src/main/java/com/example/application/data/VehicleType.java
+++ src/main/java/com/example/application/data/VehicleType.java
@@ -7,12 +7,12 @@ import org.vaadin.lineawesome.LineAwesomeIcon;
 //osobowy PKV, dostawczy LKV
 
 public enum VehicleType {
-	Car, Lorry, Motorcycle, Moped, Other;
+	Lorry, Car, Motorcycle, Moped, Other;
 
 	public Component getIcon() {
 		return switch (this) {
-			case Car -> LineAwesomeIcon.CAR_SIDE_SOLID.create();
 			case Lorry -> LineAwesomeIcon.TRUCK_SOLID.create();
+			case Car -> LineAwesomeIcon.CAR_SIDE_SOLID.create();
 			case Motorcycle, Moped -> LineAwesomeIcon.MOTORCYCLE_SOLID.create();
 			case Other -> LineAwesomeIcon.QUESTION_CIRCLE.create();
 		};
