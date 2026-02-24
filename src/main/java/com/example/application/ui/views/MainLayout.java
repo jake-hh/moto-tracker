7f70ed3d041fa6a6298ced033737618b567a94f1
jake-hh <jakob.holzer.m@gmail.com>
2026-02-25 00:04:42 +0100

    [MainLayout] Add profile avatar with menu popover

--- src/main/java/com/example/application/ui/views/MainLayout.java
+++ src/main/java/com/example/application/ui/views/MainLayout.java
@@ -1,9 +1,11 @@
 package com.example.application.ui.views;
 
+import com.example.application.data.entity.AppUser;
 import com.example.application.data.entity.Vehicle;
 import com.example.application.security.SecurityService;
 import com.example.application.services.MainService;
 import com.example.application.services.UserSettingsService;
+import com.example.application.ui.Notify;
 import com.example.application.ui.events.*;
 import com.example.application.ui.render.VehicleIconRenderer;
 import com.example.application.ui.views.dashboard.DashboardView;
@@ -12,25 +14,34 @@ import com.example.application.ui.views.service.ServiceView;
 import com.example.application.ui.views.tracker.TrackerView;
 import com.example.application.ui.views.vehicle.VehicleView;
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.HasValue.ValueChangeEvent;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.avatar.AvatarVariant;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.popover.Popover;
+import com.vaadin.flow.component.popover.PopoverPosition;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.jetbrains.annotations.NotNull;
 import org.vaadin.lineawesome.LineAwesomeIcon;
+
+import java.util.function.Consumer;
 
 
 @SpringComponent
@@ -67,10 +78,7 @@ public class MainLayout extends AppLayout {
 				LumoUtility.Margin.MEDIUM
 		);
 
-		String u = securityService.getCurrentUsername();
-		Button logout = new Button("Log out " + u, click -> securityService.logout());
-
-		var header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+		var header = new HorizontalLayout(new DrawerToggle(), logo, createAvatar());
 		header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
 		header.expand(logo);
 		header.setWidthFull();
@@ -79,6 +87,96 @@ public class MainLayout extends AppLayout {
 				LumoUtility.Padding.Horizontal.MEDIUM
 		);
 		return header;
+	}
+
+	private Component createAvatar() {
+		Avatar avatar = new Avatar();
+		avatar.addClassName("avatar");
+		avatar.getElement().setAttribute("tabindex", "-1");
+
+		Button button = new Button(avatar);
+		button.addClassName("avatar-button");
+		button.addThemeVariants(
+				ButtonVariant.LUMO_ICON,
+				ButtonVariant.LUMO_TERTIARY_INLINE
+		);
+
+		Popover popover = createPopover();
+		popover.setTarget(button);
+
+		return new HorizontalLayout(button, popover);
+	}
+
+	private Popover createPopover() {
+		var popover = new Popover();
+		popover.setModal(true);
+		popover.setOverlayRole("menu");
+		popover.setAriaLabel("User menu");
+		popover.setPosition(PopoverPosition.BOTTOM_END);
+
+		var header = new HorizontalLayout();
+		header.addClassName("popover-header");
+
+		Avatar popoverAvatar = new Avatar();
+		popoverAvatar.getElement().setAttribute("tabindex", "-1");
+		popoverAvatar.addThemeVariants(AvatarVariant.LUMO_LARGE);
+
+		header.add(popoverAvatar, createNameLayout());
+
+		var menu = new VerticalLayout();
+		menu.addClassName("popover-links");
+
+		Button profileBtn = createMenuLink("User profile", click -> Notify.debug("Profile page is under construction"));
+		Button logoutBtn = createMenuLink("Log out",  click -> securityService.logout());
+
+		menu.add(profileBtn, logoutBtn);
+
+		popover.add(header, menu);
+
+		return popover;
+	}
+
+	@NotNull
+	private VerticalLayout createNameLayout() {
+		AppUser user = securityService.getCurrentUser();
+		String username = user.getUsername();
+		String firstName = user.getFirstName();
+		String lastName = user.getLastName();
+
+		var nameLayout = new VerticalLayout();
+		nameLayout.setSpacing(false);
+		nameLayout.setPadding(false);
+		nameLayout.addClassName("popover-header-name-layout");
+
+		if (firstName != null || lastName != null) {
+			String fullName;
+
+			if (firstName != null)
+				if (lastName != null)
+					fullName = firstName + " " + lastName;
+				else
+					fullName = firstName;
+			else
+				fullName = lastName;
+
+			Span title = new Span(fullName);
+			title.addClassName("popover-header-title");
+			nameLayout.add(title);
+		}
+
+		Span subtitle = new Span(username);
+		subtitle.addClassName("popover-header-subtitle");
+		nameLayout.add(subtitle);
+
+		return nameLayout;
+	}
+
+	private Button createMenuLink(String text, Consumer<ClickEvent<Button>> onClick) {
+		Button button = new Button(text);
+		button.addClickListener(onClick::accept);
+		button.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+		button.addClassName("popover-link");
+		return button;
 	}
 
 	private Component createDrawer() {
