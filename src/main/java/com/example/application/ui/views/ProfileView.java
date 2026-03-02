426ab472b23e244c30b26268187f9636c8e3236c
jake-hh <jakob.holzer.m@gmail.com>
2026-03-02 18:40:02 +0100

    [ProfileView] Add button for editing vehicles

--- src/main/java/com/example/application/ui/views/ProfileView.java
+++ src/main/java/com/example/application/ui/views/ProfileView.java
@@ -2,13 +2,18 @@ package com.example.application.ui.views;
 
 import com.example.application.data.entity.AppUser;
 import com.example.application.security.SecurityService;
-
 import com.example.application.services.MainService;
+import com.example.application.ui.views.vehicle.VehicleView;
+
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
@@ -16,6 +21,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+import org.vaadin.lineawesome.LineAwesomeIcon;
 
 import jakarta.annotation.security.PermitAll;
 
@@ -70,11 +76,27 @@ public class ProfileView extends VerticalLayout {
 		// Vehicle counter
 		Span vehicleCounter = new Span("Vehicles: " + mainService.countVehiclesByUser(user));
 
+		// Create edit vehicles button
+		var editVehiclesBtn = new Button(LineAwesomeIcon.PEN_SOLID.create());
+		editVehiclesBtn.addThemeVariants(
+				ButtonVariant.LUMO_ICON,
+				ButtonVariant.LUMO_TERTIARY
+		);
+		editVehiclesBtn.setTooltipText("Edit vehicles");
+		editVehiclesBtn.addClickListener(click -> UI.getCurrent().navigate(VehicleView.class));
+
+		// Vehicle bar
+		var vehicleBar = new HorizontalLayout(
+				vehicleCounter,
+				editVehiclesBtn
+		);
+		vehicleBar.setAlignItems(FlexComponent.Alignment.CENTER);
+
 		// Main column
 		VerticalLayout content = new VerticalLayout(
 				avatar,
 				header,
-				vehicleCounter,
+				vehicleBar,
 				createForm()
 		);
 
